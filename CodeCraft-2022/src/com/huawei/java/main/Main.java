b0eb8ff84399d61f2f381b6dff67f84cc726b4ac
lsw <451091958@qq.com>
2022-03-19 16:07:10 +0800

    max based method

--- CodeCraft-2022/src/com/huawei/java/main/Main.java
+++ CodeCraft-2022/src/com/huawei/java/main/Main.java
@@ -44,8 +44,8 @@ public class Main {
                 siteName.add(temp[0]);
                 site_bandwidth.put(temp[0], Integer.valueOf(temp[1]));
             }
-//            System.out.println("site_bandwidth: " + site_bandwidth);
-//            System.out.println("siteName: " + siteName);
+//             System.out.println("site_bandwidth: " + site_bandwidth);
+//             System.out.println("siteName: " + siteName);
         }
         catch (IOException e) {
             e.printStackTrace();
@@ -99,12 +99,18 @@ public class Main {
             }
             //初始化qos_s
             HashMap<String, Integer> map = new HashMap<>();
-
-
-
-
+            int len_site = siteName.size();
+            int len_demand = demandName.size();
+            for (int i = 0; i < len_demand; i++) {
+                String demand_name = demandName.get(i);
+                for (int j = 0; j < len_site; j++) {
+                    map.put(siteName.get(j), qos_d.get(siteName.get(j)).get(demand_name));
+                }
+                qos_s.put(demand_name, map);
+            }
 
 //            System.out.println("qos_d: " + qos_d);
+//            System.out.println("qos_s: " + qos_s);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -135,6 +141,75 @@ public class Main {
         Arrays.stream(array).forEach(temp::add);
         return temp;
     }
+    public static HashMap<String, Integer> dispatchbasedMaxBandSite(List<Map.Entry<String, Integer>> demandMap){
+        //深复制一份 节点-剩余容量 map； 因为每个时间节点开始都是满的，所以每次都直接复制最大值。
+        // String直接使用引用进行浅复制，因为只会更改当前带宽值，所以引用String应该没关系
+        HashMap<String, Integer> site_resband = new HashMap<>();
+        for (Map.Entry<String, Integer> entry : site_bandwidth.entrySet()){
+            String s = entry.getKey();
+            int b =entry.getValue();
+            site_resband.put(s,b);
+        }
+        //System.out.println(site_resband);
+
+        //对每个用户都得到用户名和需求
+        for (Map.Entry<String, Integer> entry : demandMap){
+            String curClient = entry.getKey();
+            int curDemand = entry.getValue();
+
+            //根据用户名得到所有能连接的site，和其剩余带宽
+            //先求site
+            HashMap<String, Integer> ALLsite = qos_s.get(curClient);
+
+            //保存一个可以连接的site和其对应的剩余带宽的map
+            HashMap<String, Integer> SitecanConnect_withband = new HashMap<>();
+
+            for(Map.Entry<String, Integer> Site_qos : ALLsite.entrySet()){
+                String s = Site_qos.getKey();
+                int qos =Site_qos.getValue();
+                if(qos<=qos_constraint){
+                    SitecanConnect_withband.put(s,site_resband.get(s));
+                }
+            }
+
+            //对site根据当前容量进行排序
+            List<Map.Entry<String, Integer>> connectedSiteList = new ArrayList<>(SitecanConnect_withband.entrySet());
+            connectedSiteList.sort((o1,o2) -> o2.getValue()-o1.getValue());
+            //遍历site
+
+            for(Map.Entry<String, Integer> site_maxband :connectedSiteList){
+                String s = site_maxband.getKey();
+                int resband =site_maxband.getValue();
+                //满足 直接都放进去
+                //不满足 放完，对下一个site继续放
+                if(resband>=curDemand){
+                    resband -= curDemand;
+                    curDemand = 0;
+                }
+                else{
+                    curDemand -= resband;
+                    resband = 0;
+                }
+                //记录剩余带宽
+
+                site_resband.put(s,resband);
+
+            }
+            //到此，第一个用户的需求根据可以连接的节点的带宽从大到小分配结束
+            //site_resband记录了节点的带宽剩余情况。
+        }
+        //到此，site_resband记录了所有节点的带宽剩余情况。
+        //如果要得到答案（带宽的使用情况），则用 最大值-剩余情况
+        //最大值-剩余情况==0的话 说明该节点没有被使用，无需添加
+        HashMap<String, Integer> site_used = new HashMap<>();
+        for (Map.Entry<String, Integer> entry : site_resband.entrySet()){
+            String s = entry.getKey();
+            int b =site_bandwidth.get(s) - entry.getValue();
+            if(b==0) continue;
+            site_used.put(s,b);
+        }
+        return site_used;
+    }
 
     public static void dispatch(){
         int time_size = timeList.size();
@@ -145,7 +220,10 @@ public class Main {
         List<Map.Entry<String, Integer>> demandList = new ArrayList<>(demandMap.entrySet());
         //按需求流量的大小进行排序，排序方式为从大到小
         demandList.sort((o1,o2) -> o2.getValue()-o1.getValue());
-
+        System.out.println(demandList);
+        //调用max优先分配，返回一个节点使用情况的map
+        HashMap<String, Integer> site_used = dispatchbasedMaxBandSite(demandList);
+        System.out.println(site_used);
 
 
     }
